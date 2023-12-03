e475dd33826ff002dc80ab2af18d2c54122d81a2
Conner Clere <connerclere@gmail.com>
2023-12-02 22:13:53 -0600

    add part 2 to day 1

--- src/day1.rb
+++ src/day1.rb
@@ -1,9 +1,41 @@
 # frozen-string-literal: true
 
-total = 0
-File.open('../data/day1.txt', 'r').each_line do |line|
-  line = line.strip.gsub(/[A-Za-z]/, '')
-  line = "#{line[0]}#{line[-1]}"
-  total += line.to_i
+def part_one
+  total = 0
+  File.open('../data/day1.txt', 'r').each_line do |line|
+    line = line.strip.gsub(/[A-Za-z]/, '')
+    line = "#{line[0]}#{line[-1]}"
+    total += line.to_i
+  end
+  puts total
 end
-puts total
+
+def part_two
+  total = 0
+  File.open('../data/day1.txt', 'r').each_line do |line|
+    line = convert_line(line)
+    line = "#{line[0]}#{line[-1]}"
+    total += line.to_i
+  end
+  total
+end
+
+def convert_line(line)
+  number_array = %w[zero one two three four five six seven eight nine].freeze
+  converted_line = ''
+  line.length.times do |count|
+    number_array.each_with_index do |val, index|
+      converted_line += line[count].to_s if line[count] == index.to_s
+      line_snippet = ''
+      line_snippet = line.slice(count, val.length) unless val.length > line.length - count || !line[count].to_i.zero?
+      converted_line += index.to_s unless line_snippet.index(val).nil?
+    end
+  end
+  converted_line
+end
+
+#put 'Part 1: '
+#part_one
+
+puts 'Part 2: '
+puts part_two
